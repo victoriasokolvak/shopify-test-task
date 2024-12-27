a18d02ce54df229622686b89400938bb51a4c17e
Victoria Sokolvak <sokolvak.victoria@gmail.com>
2024-12-27 11:01:34 +0200

    add Gulp setup and configure SASS for SCSS compilation

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+gulp.task('sass', () => {
+  return gulp.src('styles/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('assets'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('styles/**/*.scss', gulp.series('sass'));
+});
